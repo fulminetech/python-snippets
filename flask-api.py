f1c3fe2cbad0e83c7dbf63ce671c82d7af31284b
marutimuthu <marutimuthu@gmail.com>
2020-12-10 22:33:55 +0530

    Minor improvements in flask api

--- flask-api.py
+++ flask-api.py
@@ -6,26 +6,46 @@ from flask_restful import reqparse, abort, Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-parser = reqparse.RequestParser()
-parser.add_argument('task')
+payload = {
+    'connection': False,
+    'pstatus': [ 1, 2],
+    'pLHS_data': [],
+    'pLHS_processed': [],
+    'pRHS_data': [],
+    'pRHS_processed': [],
+    'mLHS_data': [],
+    'mLHS_processed': [],
+    'mRHS_data': [],
+    'mRHS_processed': [],
+    'eLHS_data': [],
+    'eLHS_processed': [],
+    'eRHS_data': [],
+    'eRHS_processed': [],
+    'avg': [],
+    'stats': []
+}
 
-payload = {'name': 'Maruti'}
+VART = 10
 
 class Payload(Resource):
     def get(self):
         print('Received!')
-        return payload
+        return VART
 
 class Set(Resource):
     def get(self, parameter, value):
-        print(parameter)
-        return value
-
+        global VART
+        if parameter == "pressure":
+            VART = 100
+            return parameter+" set to "+value
+        elif parameter == "machine":
+            return value
+        else:
+            return "Invalid Command"
 
 api.add_resource(Payload, '/api/payload')
 
 api.add_resource(Set, '/set/<parameter>/<value>')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
